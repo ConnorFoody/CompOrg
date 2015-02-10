3792f7496ffc7c1b1dadb8e172b9ab37fb2617fc
ConnorFoody <connorfoody@gmail.com>
2015-02-10 11:35:49 -0500

    Create lab3_ex3.c

---
+++ src/labs/lab3/lab3_ex3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define SIZE 10
+
+/* prototypes for functions */
+float getArrayStats(int[], int, int *, int *);
+void  decrement (int[], int);
+void  printArray (int[], int);
+
+/* function main begins program execution */
+int main(void)
+{
+
+   int num = SIZE;  /* sample array and size */
+   int values[SIZE] = {10,9,8,7,6,5,4,3,2,1};
+   /* parameters for array values */
+   int max, min;
+   float average;
+
+   /* INSERT CODE HERE TO PRINT ARRAY STATISTICS */
+
+   /* print the numbers in the array before call to decrement */
+   printf("\nBefore the call to function decrement:\n");
+   printArray(values, num);
+   /* The next call changes #'s in the values array */
+   decrement(values, num);
+   /* print the numbers in the array after call to decrement */
+   printf("\nAfter the call to function decrement:\n");
+   printArray(values, num);
+
+   return 0;
+
+} /* end of main */
+
+void decrement(int values[], int num_elements)
+{
+   /* Decrements each value stored in the array */
+   /* values by 1 parameter num_elements gives  */
+   /* the # of elements in the array            */
+   int i; /* temp counter */
+   for (i = 0; i < num_elements; i++)
+      values[i]--;
+
+} /* end of decrement */
+
+void printArray(int values[], int num_elements)
+{
+   /* Prints each value stored in the array */
+   /* parameter num_elements gives the # of */
+   /* elements in the array                 */
+   int i; /* temp counter */
+   for (i = 0; i < num_elements; i++)
+      printf("%4d", values[i]);
+   printf("\n");
+
+} /* end of printArray */
+
