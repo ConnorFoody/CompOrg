cfe0a67ea968db3988217518625705cca55c8a67
connorfoody <connorfoody>
2015-03-14 18:45:11 -0400

    got the second part of HW3 working

--- src/HW/HW3/hw3_1.s
+++ src/HW/HW3/hw3_1.s
@@ -1,7 +1,6 @@
 	.data 
 	.space 32
 prompt: .asciiz "Type in a positive integer: "
-in_loop: .asciiz "in loop \n"
 at_end:	.asciiz "done\n"
 newln_char: .asciiz "\n"
 	
@@ -27,19 +26,18 @@ main:
 	addi $sp, $sp, -4	# make room for one integer
 	sw $v0, 0 ($sp)		# put the integer onto the stack
 
-	jal find_num_zeros 	# jump to num zeros
-	j print			# jump over to print	
+	jal find_num_zeros_right # find the number of zeros on the right side of the number
+	jal find_num_ones_left	# find the number of ones on the left of the number
+	jal print		# jump over to print	
+	j exit			# exit
+
 
 ########################################################################
 ########################################################################
-find_num_zeros:
+find_num_zeros_right:
 	# find the number of zeros in the right half of number
 	# bitwise and the input number with 65535 to isolate the right side
 	
-	# t0 --> original number
-	# t1 --> bitwise and'ed number
-	# t2 --> counter for number of zeros
-	# t3 --> loop counter
 	lw $t0, 0 ($sp)		# load the number off the stack
 	andi $a0, $t0, 65535	# bitwise and it to isolate the right side
 
@@ -52,16 +50,36 @@ find_num_zeros:
 	lw $31, 0 ($sp)		# load return location off of the stack
 	addi $sp, $sp, 4	# clean the stack up
 	
-	# print for testing 
-	lw $a0, 0 ($sp)
-	li $v0, 1
-	syscall
-
-	la $a0, newln_char
-	li $v0, 4
-	syscall 
+	# subtrack the number we found from 16 to find number of zeros
+	lw $t0, 0 ($sp)		# load number to temp register
+	li $t1, 16		# constant to subtract from
+	sub $t0, $t1, $t0	# subtrack number of ones from 16
+	sw $t0, 0 ($sp)		# save number of zeros to the stack
 
 	jr $31			# return back up
+
+########################################################################
+########################################################################
+find_num_ones_left:
+	# find the number of ones in the right half of the number
+	
+	lw $t0, 4 ($sp)		# load original number off the stack
+	li $t1, 65535		# mask to isolate right side (number is shifted over so left becomes right)
+	srl $t0, $t0, 16	# shift number over because find_num_ones checks 1st 16 bits
+	
+	and $a0, $t0, $t1	# do bitwise and on input number
+
+	addi $sp, $sp, -8	# make room on stack for return address and new number
+	sw $31, 0 ($sp)		# put return address on stack
+
+	jal find_num_ones	# go look for the number of ones
+
+	lw $31, 0 ($sp)		# load return location off the stack
+	addi $sp, $sp, 4	# clean the stack up
+
+	jr $31			# return back up 
+
+	# make room on stack for jump register and new number
 
 ########################################################################
 ########################################################################
@@ -94,32 +112,40 @@ end_loop:
 	sw $t0, 4 ($sp)		# put num zeros onto the stack
 	
 	# print for testing
-	lw $a0, 4 ($sp)
-	li $v0, 1
-	syscall
+	#lw $a0, 4 ($sp)
+	#li $v0, 1
+	#syscall
 
 	# print newline string
-	la $a0, newln_char
-	li $v0, 4
-	syscall
+	#la $a0, newln_char
+	#li $v0, 4
+	#syscall
 	
 	jr $31
  
 ########################################################################
 ########################################################################
 print:
-	# print all the data
+	# print number of zeros on the right
 	lw $a0, 0($sp)		# load data off the stack
 	addi $sp, $sp, 4	# clean the stack back up
 	li $v0, 1		# code for printing integer
 	syscall
 
+	# print new line
+	la $a0, newln_char	# load newline character
+	li $v0, 4		# code to print string
+	syscall
+	
+	jr $31			# return back to main
+
+
+exit: 
 	# print finished message
 	la $a0, at_end
 	li $v0, 4
 	syscall
-
-exit: 
+	
 	# exit cleanly once done
 	li $v0, 10
 	syscall
