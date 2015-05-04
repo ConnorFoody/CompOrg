25ed7868ecd97b03aa6a0bc01cc3e8e123bf4135
connorfoody <connorfoody>
2015-05-04 12:57:58 -0400

    added all the comments, cleaning up now

--- src/HW/bonusHW/bonus.c
+++ src/HW/bonusHW/bonus.c
@@ -29,8 +29,10 @@ struct identifier_reference{
 	struct line_value* head;
 };
 
-int analyze_file(FILE* infp, FILE* outfp);
+// prototype for function called by main
+void analyze_file(FILE* infp, FILE* outfp);
 
+// main program function
 int main(int argc, char** argv){
 	printf("hello world\n");
 
@@ -80,39 +82,61 @@ int main(int argc, char** argv){
 	}
 
 	return 0;	
-}
+} // end main
 
+// finds the number of digits in a positive number
+// inputs: 
+// 	number - int, number to find the number of digits for
+//
+// output: 
+// 	int, the number of digits in the input number
 int find_num_digits(int number){
-	int tmp = 0; 			// holder
-	
-}
-char* add_line_number(char* line, int number){
-	char* to_add; 				// line to add	
+	if(number < 10) { return 1; } 
+	return 1 + find_num_digits(number / 10);	
+} // end find_num_digits
 
-	int size_of_int = floor(log10((double)abs(number))) + 1;
-	// alloc the memory 
-	if((to_add = (char*) malloc((5 + strlen(line))* sizeof(char))) == NULL){
+// adds the line number to the front of the input string
+// inputs: 
+// 	line - string, line to add the line number to
+// 	number - int, the line number for the input string
+//
+// outputs:
+//  string, the input string with the line number and a tab insert on the front
+char* add_line_number(char* line, int number){
+	char* to_add; // line we will return	
+	
+	// find how many spaces we need to allocate for the number and the tab
+	int size_of_number = find_num_digits(number) + 1;
+	
+	// size of the string is the legnth of the input string + the length of the number string + breathing room	
+	if((to_add = (char*) malloc((2 + size_of_number + strlen(line))* sizeof(char))) == NULL){
 		fprintf(stderr, "ERROR: could not alloc enough memory for line string\n");
 		exit(1);
 	}
 
-	// print the string and concat
+	// print the line numbers in then concat the old string on
 	sprintf(to_add, "%d\t", number);
 	strcat(to_add, line);
 
-	// return the new line
 	return to_add;
-}
+} // end add_line_number
 
-void check_identifier(char* token, struct identifier_reference* identifiers, int* num_identifiers, int line_number){
+// checks if an identifier is in the list and updates use lines accordingly
+// inputs: 
+// 	token - string, the potential identifier
+// 	identifiers - identifier_reference array, identifier data
+// 	num_identifiers - int, how many identifiers we have found so far
+// 	line_number - int, which line the token is on
+//
+// output:
+//  none
+void check_identifier(char* token, struct identifier_reference* identifiers, int num_identifiers, int line_number){
 	// checks if a token is an identifier
 	int i = 0; 
 
-	//printf("\t\tchecking if \"%s\" is an identifier\n", token);
-	while(i != *num_identifiers){
+	while(i != num_identifiers){
 		if(strcmp(identifiers[i].name, token) == 0){
 			// found an identifier already in the list, add it
-			//printf("\t\ttoken \"%s\" is an identifier, updating table\n", token);
 
 			// alloc new line_value
 			struct line_value* tmp;
@@ -120,12 +144,14 @@ void check_identifier(char* token, struct identifier_reference* identifiers, int
 				fprintf(stderr, "ERROR: could not alloc mem for line_value\n");
 				exit(1);
 			}
-			struct line_value* head = identifiers[i].head;
 			tmp->next = NULL;
 			tmp->value = line_number;
 
-			// check if we are adding the first line
+			// get the head of the line_value LL
+			struct line_value* head = identifiers[i].head;
+
 			if(head == NULL){
+				// if the identifier has not yet been used, add it as the head 
 				identifiers[i].head = tmp;
 				return;
 			}
@@ -141,9 +167,17 @@ void check_identifier(char* token, struct identifier_reference* identifiers, int
 		}
 		i++;
 	}
-	
-}
+} // end check_identifier
 
+// adds a new identifier definition
+// inputs:
+// 	identifier - string, the potential identifier
+// 	identifiers - identifier_reference array, the identifier data
+// 	num_identifiers - int, how many identifiers have been found so far. Pass by reference so it can be incremented
+// 	line_number - int, which line the identifier is on
+//
+// output:
+// 	none
 void add_new_identifier(char* identifier, struct identifier_reference* identifiers, int* num_identifiers, int line_number){
 	int i = 0; 
 	
@@ -156,25 +190,27 @@ void add_new_identifier(char* identifier, struct identifier_reference* identifie
 	if(identifier[strlen(identifier) - 1] != ':'){
 		printf("\tWARNING: expected token to end in ':'\n");
 	}
+	// chop the ':' off the identifier with a null termination
 	identifier[strlen(identifier) - 1] = '\0';
 	
+	// look for the identifier	
 	while(i != *num_identifiers){
 		if(strcmp(identifiers[i].name, identifier) == 0){
-			// found an identifier already in the list, add it
+			// found an identifier already in the list
+			// should not happen b/c it would mean a redefinition
 			printf("\t\tWARNING: identifier found in table\n");
 			return; 
 		}
 		i++;
 	}
 
-	//printf("\t\tidentifier is not already in the list, adding it in: %s\n", identifier);
 	// build and add new identifier
 	struct identifier_reference tmp; 
 	if((tmp.name = (char*) malloc(11 * sizeof(char))) == NULL){ // give the right ammount of space
 		fprintf(stderr, "ERROR: could not alloc memory to add a new identifier\n");
 		exit(1);
 	}
-	strcpy(tmp.name, identifier); // need to check for error?
+	strcpy(tmp.name, identifier);
 	tmp.head = NULL;
 	tmp.definition = line_number;
 	
@@ -185,10 +221,19 @@ void add_new_identifier(char* identifier, struct identifier_reference* identifie
 	(*num_identifiers)++;
 
 	return;
-}
+} // end add_new_identifier
 
+// processes the line, looking for identifier definitions or use lines
+// inputs: 
+// 	line - string, the line read from the file
+// 	identifiers - identifier_reference array, the identifier data
+// 	num_identifiers - int, how many identifiers have been found so far
+// 	line_number - int, which line was read from the input file
+// 	scan_definition - bool, 1 = looking for definitions only, 0 = looking for use lines only
+//
+// output:
+// 	none
 void analyze_line(char* line, struct identifier_reference* identifiers, int* num_identifiers, int line_number, int scan_definition){
-	// analyzes the line
 		
 	// deliminate by commas and spaces	
 	char* tkn = strtok(line, ",\n \t");
@@ -212,28 +257,39 @@ void analyze_line(char* line, struct identifier_reference* identifiers, int* num
 		}
 
 		printf("\tlooking at token: %s\n", tkn);	
+		// null terminate the token
 		if(tkn[strlen(tkn) - 1] == '\n'){
 			printf("\tcaught new line, going to fix\n");
 			tkn[strlen(tkn) - 1] = '\0';
 		}
 
 		if(tkn[strlen(tkn) - 1] == ':' && scan_definition){
+			// if we found a definition and we are looking for definitions
 			printf("\t\tfound an identifier definition: %s\n", tkn);
 			add_new_identifier(tkn, identifiers, num_identifiers, line_number);
 		}	
 		else if(!scan_definition){
-			check_identifier(tkn, identifiers, num_identifiers, line_number);
+			// if we are looking for use lines, check if the token is an identifier
+			check_identifier(tkn, identifiers, *num_identifiers, line_number);
 		}
-		//printf("\tlast char is: |%c|\n", tkn[strlen(tkn) -1 ]);
 
+		// get the next token
 		tkn = strtok(NULL, ", \t\n");
 	}
-}
+} // end analyze line
 
+// prototypes for functions called by analyze_file
 void print_table(FILE* outfp, struct identifier_reference* identifiers, int num_identifiers);
 void free_identifiers(struct identifier_reference* identifiers, int num_identifiers);
 
-int analyze_file(FILE* infp, FILE* outfp){
+// reads from the input file, analyzes the info, then writes to the output file
+// inputs:
+// 	infp - the input file to be processed
+// 	outfp - the output file to be written to
+// 
+// outputs:
+// 	none
+void analyze_file(FILE* infp, FILE* outfp){
 	// reads the input file, puts line numbers on the front then moves on
 
 	int line_count = 1;		// track line numbers
@@ -274,8 +330,6 @@ int analyze_file(FILE* infp, FILE* outfp){
 		// copy the line so analyze_line and it's subfunctions can change it
 		strcpy(line_cpy, in_line);		
 
-		printf("INLINE: %s\n", in_line);
-
 		if(strlen(in_line) > 80){
 			printf("ERROR: inline is too long\n");
 			exit(1);
@@ -307,7 +361,7 @@ int analyze_file(FILE* infp, FILE* outfp){
 
 		// read in the input line		
 		if(fgets(in_line, 80, infp) == NULL){
-			printf("at end of file while scanning for use lines\n");
+			// if at the end of the input file, break
 			break;
 		}
 
@@ -325,16 +379,22 @@ int analyze_file(FILE* infp, FILE* outfp){
 	}
 
 	// write table
-	printf("Going to write file\n");
 	print_table(outfp, identifiers, num_identifiers);
 
 	// clean up the memory
 	free(in_line);
 	free_identifiers(identifiers, num_identifiers);
 	free(line_cpy);
-	return 0;
-}
+} // end analyze_file
 
+// prints the identifier table at the end of the output file
+// inputs: 
+// 	outfp - file pointer to the output file
+// 	identifiers - array of identifier_reference structs, holds the identifier data
+// 	num_identifiers - int number of identifiers in the array
+//
+// outputs:
+// 	none
 void print_table(FILE* outfp, struct identifier_reference* identifiers, int num_identifiers){
 	// prints out a table with the identifiers on it at the end
 	char* to_print;
@@ -386,16 +446,27 @@ void print_table(FILE* outfp, struct identifier_reference* identifiers, int num_
 	// make sure to free memory
 	free(to_print);
 
-}
+} // end print_table
 
-// helper function for freeing line_value LL in identifier_reference 
+// frees a linked list of use lines
+// inputs: 
+// 	node - line_value pointer, node to be freed
+// 
+// outputs:
+// 	none
 void free_linked_list(struct line_value* node){
 	if(node == NULL){ return; }
 	free_linked_list(node->next);
 	free(node);
-}
+} // end free_linked_list
 
-// helper function to free the identifier_reference array b/c its a little tricky
+// helper function to free the identifier_reference array 
+// inputs:
+// 	identifiers - identifier_reference array, the identifier data
+// 	num_identifiers - int, number of identifiers found
+//
+// outputs:
+// 	none
 void free_identifiers(struct identifier_reference* identifiers, int num_identifiers){
 	int i; 			// loop counter
 
@@ -408,4 +479,4 @@ void free_identifiers(struct identifier_reference* identifiers, int num_identifi
 	}
 	// free the array it's self
 	free(identifiers);
-}
+} // end free_identifiers
